879c6cc0be704a0b4e0953dd6ae49fe2bacdb005
nilson <nilson.dyrmishi@gmail.com>
2022-02-02 13:24:11 -0500

    Commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import NavBar from "../components/NavBar";
 import Announcement from "../components/Annoucement";
-
+import Slider from "../components/Slider";
+import Categories from "../components/Categories";
 function Home(){
     return(<div>
             <Announcement/>
             <NavBar/>
+            <Slider/>
+            <Categories/>
         </div>
 
     )
